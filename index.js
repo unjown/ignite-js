6e1cff73424c01d13f643e99109b79bba94dcacb
unjown <71381907+unjown@users.noreply.github.com>
2021-10-06 17:45:30 +0800

    add akinator

--- index.js
+++ index.js
@@ -1,111 +1,123 @@
-	const { Client, Intents } = require('discord.js');
-	const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES] });
-	const config = require('./config.json');
-	const zoom =  require('./zoom.json')
-	const link = (zoom.zoomL)
-	var CronJob = require('cron').CronJob;
-	const AntiSpam = require('discord-anti-spam');
-	const antiSpam = new AntiSpam({
-		warnThreshold: 5, // Amount of messages sent in a row that will cause a warning.
-		muteThreshold: 10, // Amount of messages sent in a row that will cause a mute
-		kickThreshold: 10000, // Amount of messages sent in a row that will cause a kick.
-		banThreshold: 100000, // Amount of messages sent in a row that will cause a ban.
-		maxInterval: 3000, // Amount of time (in milliseconds) in which messages are considered spam.
-		warnMessage: '{@user}, Kindly stop spamming or severe punishments will happen!', // Message that will be sent in chat upon warning a user.
-		muteMessage: '**{user_tag}** has been muted for spamming uncontrollably.',// Message that will be sent in chat upon muting a user.
-		maxDuplicatesWarning: 3, // Amount of duplicate messages that trigger a warning.
-		maxDuplicatesKick: 1000, // Amount of duplicate messages that trigger a warning.
-		maxDuplicatesBan: 1200, // Amount of duplicate messages that trigger a warning.
-		maxDuplicatesMute: 6, // Ammount of duplicate message that trigger a mute.
-		ignoredPermissions: [ 'ADMINISTRATOR'], // Bypass users with any of these permissions.
-		ignoreBots: true, // Ignore bot messages.
-		verbose: true, // Extended Logs from module.
-		ignoredMembers: [], // Array of User IDs that get ignored.
-		muteRoleName: "Lockdown", // Name of the role that will be given to muted users!
-		removeMessages: false
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES] });
+const config = require('./config.json');
+const akinator = require("discord.js-akinator");
+const zoom =  require('./zoom.json')
+const link = (zoom.zoomL)
+var CronJob = require('cron').CronJob;
+const AntiSpam = require('discord-anti-spam');
+const antiSpam = new AntiSpam({
+	warnThreshold: 5, // Amount of messages sent in a row that will cause a warning.
+	muteThreshold: 10, // Amount of messages sent in a row that will cause a mute
+	kickThreshold: 10000, // Amount of messages sent in a row that will cause a kick.
+	banThreshold: 100000, // Amount of messages sent in a row that will cause a ban.
+	maxInterval: 3000, // Amount of time (in milliseconds) in which messages are considered spam.
+	warnMessage: '{@user}, Kindly stop spamming or severe punishments will happen!', // Message that will be sent in chat upon warning a user.
+	muteMessage: '**{user_tag}** has been muted for spamming uncontrollably.',// Message that will be sent in chat upon muting a user.
+	maxDuplicatesWarning: 3, // Amount of duplicate messages that trigger a warning.
+	maxDuplicatesKick: 1000, // Amount of duplicate messages that trigger a warning.
+	maxDuplicatesBan: 1200, // Amount of duplicate messages that trigger a warning.
+	maxDuplicatesMute: 6, // Ammount of duplicate message that trigger a mute.
+	ignoredPermissions: [ 'ADMINISTRATOR'], // Bypass users with any of these permissions.
+	ignoreBots: true, // Ignore bot messages.
+	verbose: true, // Extended Logs from module.
+	ignoredMembers: [], // Array of User IDs that get ignored.
+	muteRoleName: "Lockdown", // Name of the role that will be given to muted users!
+	removeMessages: false
 });
 
-	var bad = ['fuck' , 'bitch' , 'fck' , 'btch' , 'vagina', 'dick', 'shit' ,  'sht' , 'gago' , 'shet' , 'tangina' , 'putangina' , 'sh1t' , 'pakshet', 'pakyu']
+var bad = ['fuck' , 'bitch' , 'fck' , 'btch' , 'vagina', 'dick', 'shit' ,  'sht' , 'gago' , 'shet' , 'tangina' , 'putangina' , 'sh1t' , 'pakshet', 'pakyu']
 
-
-	client.on('messageCreate', message => antiSpam.message(message)); 
-	client.on("messageCreate", message => { 
-		if(bad.some(word => message.content.toLowerCase().includes(word))){
-			const splitMessage = message.content.split(" ");
-			if(splitMessage.some(word => message.content.toLowerCase().includes(word))){
-				message.delete()
-				const auhor = message.author.id
-				message.channel.send({content: '<@' + auhor + '> ' + 'swearing is not allowed here :)'})
-			}
-
-
-		}else if(message.content.includes('https://shellshock.io/')){
-			message.channel.send({content: "<@&885496383900102667> SHELL SHOCKERS LINK HAS BEEN SENT"})
-		
-		}else if(message.content.toLowerCase().startsWith("ignt rules")){
-		message.channel.send({content: "1. No fighting, swearing, or anything that might disturb peace in our land.\n2. No ruining of other people's houses, stealing, and killing.\n3. No trespassing! Ask the people who built the place first before entering their premises.\n4. Treat Ignite's MC server as a community that helps each other! Time to apply what you've learned in Convenience"})
-
-		}else if(message.content.toLowerCase().includes("ignt uptime")){
-			let totalSeconds = (client.uptime / 1000);
-			let days = Math.floor(totalSeconds / 86400);
-			totalSeconds %= 86400;
-			let hours = Math.floor(totalSeconds / 3600);
-			totalSeconds %= 3600;
-			let minutes = Math.floor(totalSeconds / 60);
-			let seconds = Math.floor(totalSeconds % 60);
-			let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
-			message.channel.send({content: uptime})
-
-		}else if(message.content.toLowerCase().startsWith("ignt ip")){
-			message.channel.send({content: "**--IGNITE OFFICIAL MINECRAFT SERVER--**\nIP: us-01.infernalnodes.com:25642"})
+client.on('messageCreate', message => antiSpam.message(message)); 
+client.on("messageCreate", message => { 
+	if(bad.some(word => message.content.toLowerCase().includes(word))){
+		const splitMessage = message.content.split(" ");
+		if(splitMessage.some(word => message.content.toLowerCase().includes(word))){
+			message.delete()
+			const auhor = message.author.id
+			message.channel.send({content: '<@' + auhor + '> ' + 'swearing is not allowed here :)'})
 		}
-	});
 
 
-	client.on("guildMemberAdd", (member) => {
-		console.log(member);
-	});
-	const job = new CronJob('00 55 8 * * 0', function() {
-		const d = new Date();
-		client.channels.cache.get('876363970108334162').send({content: "@everyone REMINDER: IGNITE SUNDAYS WILL BE OFFICIALLY STARTING IN 5 MINS\nZOOM LINK:" + link})
-	},
-	null,
-	true,
-	'Asia/Manila'
-						);
+	}else if(message.content.includes('https://shellshock.io/')){
+		message.channel.send({content: "<@&885496383900102667> SHELL SHOCKERS LINK HAS BEEN SENT"})
+	
+	}else if(message.content.toLowerCase().startsWith("ignt rules")){
+	message.channel.send({content: "1. No fighting, swearing, or anything that might disturb peace in our land.\n2. No ruining of other people's houses, stealing, and killing.\n3. No trespassing! Ask the people who built the place first before entering their premises.\n4. Treat Ignite's MC server as a community that helps each other! Time to apply what you've learned in Convenience"})
 
-	const job2 = new CronJob('00 30 8 * * 0', function() {
-		const d = new Date();
-		client.channels.cache.get('876363970108334162').send({content: "@everyone REMINDER: IGNITE SUNDAYS WILL BE OFFICIALLY STARTING IN 30 MINS\nZOOM LINK:" + link})
-	},
-	null,
-	true,
-	'Asia/Manila'
-	);
+	}else if(message.content.toLowerCase().includes("ignt uptime")){
+		let totalSeconds = (client.uptime / 1000);
+		let days = Math.floor(totalSeconds / 86400);
+		totalSeconds %= 86400;
+		let hours = Math.floor(totalSeconds / 3600);
+		totalSeconds %= 3600;
+		let minutes = Math.floor(totalSeconds / 60);
+		let seconds = Math.floor(totalSeconds % 60);
+		let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+		message.channel.send({content: uptime})
 
-	const job3 = new CronJob('00 30 19 * * 5', function() {
-		const d = new Date();
-		client.channels.cache.get('876363970108334162').send({content: "@everyone REMINDER: FRIDAYS SUNDAYS WILL BE OFFICIALLY STARTING IN 30 MINS\nZOOM LINK:" + link})
-	},
-	null,
-	true,
-	'Asia/Manila'
-	);
+	}else if(message.content.toLowerCase().startsWith("ignt ip")){
+		message.channel.send({content: "**--IGNITE OFFICIAL MINECRAFT SERVER--**\nIP: us-01.infernalnodes.com:25642"})
+	}else if(message.content.toLowerCase().startsWith("ignt aki")){
+		const language = "en"; //The Language of the Game
+		const childMode = true; //Whether to use Akinator's Child Mode
+		const gameType = "character"; //The Type of Akinator Game to Play. ("animal", "character" or "object")
+		const useButtons = true; //Whether to use Discord's Buttons
 
-	const job4 = new CronJob('00 55 19 * * 5', function() {
-		const d = new Date();
-		client.channels.cache.get('876363970108334162').send({content: "@everyone REMINDER: IGNITE FRIDAYS WILL BE OFFICIALLY STARTING IN 5 MINS\nZOOM LINK:" + link})
-	},
-	null,
-	true,
-	'Asia/Manila'
-	);
-
-	job.start();
-	job2.start();
-	job3.start();
-	job4.start();
+		akinator(message, {
+			language: language, //Defaults to "en"
+			childMode: childMode, //Defaults to "false"
+			gameType: gameType, //Defaults to "character"
+			useButtons: useButtons //Defaults to "false"
+		});
+	}
+});
 
 
+client.on("guildMemberAdd", (member) => {
+	console.log(member);
+});
+const job = new CronJob('00 55 8 * * 0', function() {
+	const d = new Date();
+	client.channels.cache.get('876363970108334162').send({content: "@everyone REMINDER: IGNITE SUNDAYS WILL BE OFFICIALLY STARTING IN 5 MINS\nZOOM LINK:" + link})
+},
+null,
+true,
+'Asia/Manila'
+					);
 
-	client.login(config.token);
+const job2 = new CronJob('00 30 8 * * 0', function() {
+	const d = new Date();
+	client.channels.cache.get('876363970108334162').send({content: "@everyone REMINDER: IGNITE SUNDAYS WILL BE OFFICIALLY STARTING IN 30 MINS\nZOOM LINK:" + link})
+},
+null,
+true,
+'Asia/Manila'
+);
+
+const job3 = new CronJob('00 30 19 * * 5', function() {
+	const d = new Date();
+	client.channels.cache.get('876363970108334162').send({content: "@everyone REMINDER: FRIDAYS SUNDAYS WILL BE OFFICIALLY STARTING IN 30 MINS\nZOOM LINK:" + link})
+},
+null,
+true,
+'Asia/Manila'
+);
+
+const job4 = new CronJob('00 55 19 * * 5', function() {
+	const d = new Date();
+	client.channels.cache.get('876363970108334162').send({content: "@everyone REMINDER: IGNITE FRIDAYS WILL BE OFFICIALLY STARTING IN 5 MINS\nZOOM LINK:" + link})
+},
+null,
+true,
+'Asia/Manila'
+);
+
+job.start();
+job2.start();
+job3.start();
+job4.start();
+
+
+
+client.login(config.token);
